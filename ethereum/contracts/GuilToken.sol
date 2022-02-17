ae05f63a8c163d5403510f8711caaa78c7efa9c6
guillaume-boutin <guillaumeboutin2101@gmail.com>
2022-02-16 22:38:37 -0500

    tests passing for GuilToken.transferFrom

--- ethereum/contracts/GuilToken.sol
+++ ethereum/contracts/GuilToken.sol
@@ -23,7 +23,6 @@ contract GuilToken {
 
   function transfer (address _to, uint _value) public returns (bool success) {
     bool _result = _handleTransfer(msg.sender, _to, _value);
-
     emit Transfer(msg.sender, _to, _value);
 
     return true;
@@ -33,6 +32,7 @@ contract GuilToken {
     _subtractFrom(msg.sender, _value);
     allowance[msg.sender][_to] = allowance[msg.sender][_to].add(_value);
     emit Approve(msg.sender, _to, _value);
+
     return true;
   }
 
@@ -41,6 +41,16 @@ contract GuilToken {
     _subtractFromAllowance(msg.sender, _to, _value);
     _addTo(msg.sender, _value);
     emit Unapprove(msg.sender, _to, _value);
+
+    return true;
+  }
+
+  function transferFrom(address _from, address _to, uint _value) public returns (bool success) {
+    require(_from != address(0));
+    require(_to != address(0));
+
+    _subtractFromAllowance(_from, msg.sender, _value);
+    _addTo(_to, _value);
 
     return true;
   }
@@ -57,18 +67,21 @@ contract GuilToken {
 
   function _addTo(address _subject, uint _value) internal returns (bool success) {
     balanceOf[_subject] = balanceOf[_subject].add(_value);
+
     return true;
   }
 
   function _subtractFrom(address _subject, uint _value) internal returns (bool success) {
     require(balanceOf[_subject] >= _value);
     balanceOf[_subject] = balanceOf[_subject].sub(_value);
+
     return true;
   }
 
   function _subtractFromAllowance(address _allocator, address _subject, uint _value) internal returns (bool success) {
     require(allowance[_allocator][_subject] >= _value);
     allowance[_allocator][_subject] = allowance[_allocator][_subject].sub(_value);
+
     return true;
   }
 }
---
+++ test/GuilToken/transferFrom.test.js
@@ -0,0 +1,45 @@
+import { assert } from "chai";
+import { EVM_REVERT, toWei } from "../helpers";
+
+const GuilTokenContract = artifacts.require("./GuilToken");
+
+contract("GuilToken", ([deployer, user1, user2, user3]) => {
+  let contract;
+
+  beforeEach(async () => {
+    contract = await GuilTokenContract.new();
+    await contract.transfer(user1, toWei(100), { from: deployer });
+    await contract.approve(user2, toWei(100), { from: user1 });
+  });
+
+  it("it transfers a delegated amount to another address", async () => {
+    await contract.transferFrom(user1, user3, toWei(30), { from: user2 });
+
+    const user3Balance = await contract.balanceOf(user3);
+    assert.equal(user3Balance.toString(), toWei(30));
+
+    const allowance = await contract.allowance(user1, user2);
+    assert.equal(allowance.toString(), toWei(70));
+  });
+
+  it("rejects an invalid From address", async () => {
+    try {
+      await contract.transferFrom(0x0, user3, toWei(30), { from: user2 });
+      assert.fail(EVM_REVERT);
+    } catch (e) {}
+  });
+
+  it("rejects an invalid To address", async () => {
+    try {
+      await contract.transferFrom(user1, 0x0, toWei(30), { from: user2 });
+      assert.fail(EVM_REVERT);
+    } catch (e) {}
+  });
+
+  it("rejects an amount greater than the allowance", async () => {
+    try {
+      await contract.transferFrom(user1, user3, toWei(200), { from: user2 });
+      assert.fail(EVM_REVERT);
+    } catch (e) {}
+  });
+});
