bd15aebbb12cf3b19e0e175506e20544a960d977
guillaume-boutin <guillaumeboutin2101@gmail.com>
2022-03-12 17:43:18 -0500

    kovan configs

--- truffle-config.js
+++ truffle-config.js
@@ -1,6 +1,8 @@
 require("babel-register");
 require("babel-polyfill");
 require("dotenv").config();
+const HDWalletProvider = require("truffle-hdwallet-provider-privkey");
+const privateKeys = process.env.PRIVATE_KEYS || "";
 
 module.exports = {
   networks: {
@@ -8,6 +10,17 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*",
+    },
+    kovan: {
+      provider: function () {
+        return new HDWalletProvider(
+          privateKeys.split(","),
+          `https://kovan.infura.io/v3/${process.env.INFURA_PROJECT_ID}`
+        );
+      },
+      gas: 5000000,
+      gasPrice: 25000000000,
+      network_id: 42,
     },
   },
   contracts_directory: "./ethereum/contracts/",
