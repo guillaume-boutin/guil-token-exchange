0308ca83c58522cb37d9a7805db82a577f5ddef1
guillaume-boutin <guillaumeboutin2101@gmail.com>
2022-03-13 15:44:50 -0400

    PriceChart component on mobx

--- components/content/price-chart/PriceChart.js
+++ components/content/price-chart/PriceChart.js
@@ -2,28 +2,26 @@ import { Card, CardBody, CardHeader } from "../../common/card";
 import styles from "./PriceChart.module.scss";
 import dynamic from "next/dynamic";
 import { chartOptions, dummyData } from "./config";
-import { connect } from "../../../context";
-import { Component } from "../../Component";
-import { Trade } from "../../../entities/Trade";
 import _groupBy from "lodash/groupBy";
 import moment from "moment";
 import { Spinner } from "../../common/spinner";
+import { useContext } from "react";
+import { Context } from "../../../context";
+import { observer } from "mobx-react-lite";
 
 const Chart = dynamic(() => import("react-apexcharts"), { ssr: false });
 
-/**
- * @property {Trade[]} props.trades
- * @property {boolean} props.tradesLoading
- */
-class PriceChartComponent extends Component {
-  get groupedTrades() {
-    return _groupBy(this.props.trades, (t) =>
-      t.timestamp.startOf("hour").unix()
-    );
-  }
+const PriceChartComponent = () => {
+  const { ordersStore } = useContext(Context);
 
-  get candlesticks() {
-    return Object.values(this.groupedTrades).map((trades) => {
+  const isLoading = ordersStore.trades === null;
+
+  const groupedTrades = _groupBy(ordersStore.trades, (t) =>
+    t.timestamp.startOf("hour").unix()
+  );
+
+  const candlesticks = (() => {
+    return Object.values(groupedTrades).map((trades) => {
       const timeSort = [...trades].sort((a, b) =>
         a.timestamp.isBefore(b.timestamp) ? -1 : 1
       );
@@ -38,101 +36,66 @@ class PriceChartComponent extends Component {
         timeSort[timeSort.length - 1].order.price.precision(4),
       ];
     });
-  }
+  })();
 
-  get serie() {
-    const candlesticks = this.candlesticks;
+  const serie = Object.keys(groupedTrades).map((unixTimestamp, index) => ({
+    x: moment.unix(unixTimestamp),
+    y: candlesticks[index],
+  }));
 
-    return Object.keys(this.groupedTrades).map((unixTimestamp, index) => ({
-      x: moment.unix(unixTimestamp),
-      y: candlesticks[index],
-    }));
-  }
+  const lastTwoTrades = [...ordersStore.trades]
+    .sort((a, b) => (a.timestamp.isBefore(b.timestamp) ? -1 : 1))
+    .slice(-2);
 
-  /**
-   * @return {Trade[]}
-   */
-  get lastTwoTrades() {
-    return this.props.trades
-      .sort((a, b) => (a.timestamp.isBefore(b.timestamp) ? -1 : 1))
-      .slice(-2);
-  }
+  const lastTrade = lastTwoTrades[1] ?? null;
 
-  /**
-   * @return {Trade|null}
-   */
-  get lastTrade() {
-    return this.lastTwoTrades[1] ?? null;
-  }
+  const lastPrice = lastTrade ? lastTrade.order.price.toPrecision(4) : "";
 
-  get lastPrice() {
-    const lastTrade = this.lastTrade;
-
-    if (!lastTrade) return "";
-
-    return lastTrade.order.price.toPrecision(4);
-  }
-
-  get latestPriceChange() {
-    const lastTwoTrades = this.lastTwoTrades;
+  const lastPriceChange = (() => {
     if (lastTwoTrades.length !== 2) return 0;
 
-    const [secondLastTrade, lastTrade] = this.lastTwoTrades;
+    const [secondLastTrade, lastTrade] = lastTwoTrades;
 
     return lastTrade.order.price
       .minus(secondLastTrade.order.price)
       .toPrecision(4);
-  }
+  })();
 
-  get isLoading() {
-    return this.props.tradesLoading;
-  }
-
-  render() {
-    const latestPriceChange = this.latestPriceChange;
-
-    const caret =
-      latestPriceChange < 0 ? (
-        <span className={styles.sell}>&#9660;</span>
-      ) : (
-        <span className={styles.buy}>&#9650;</span>
-      );
-
-    return (
-      <Card className={styles.card}>
-        <CardHeader>
-          <h3>Price Chart</h3>
-        </CardHeader>
-
-        <CardBody className={styles.cardBody}>
-          {this.isLoading && <Spinner />}
-
-          {!this.isLoading && (
-            <>
-              <h4 className={styles.priceTitle}>
-                GUIL/ETH {caret} {this.lastPrice}
-              </h4>
-
-              <div className={styles.priceChart}>
-                <Chart
-                  options={chartOptions}
-                  series={[{ data: this.serie }]}
-                  type="candlestick"
-                  height="100%"
-                />
-              </div>
-            </>
-          )}
-        </CardBody>
-      </Card>
+  const caret =
+    lastPriceChange < 0 ? (
+      <span className={styles.sell}>&#9660;</span>
+    ) : (
+      <span className={styles.buy}>&#9650;</span>
     );
-  }
-}
 
-export const PriceChart = connect(
-  ({ exchange }) => ({
-    trades: exchange.trades,
-    tradesLoading: exchange.tradesLoading,
-  }),
-  PriceChartComponent
-);
+  return (
+    <Card className={styles.card}>
+      <CardHeader>
+        <h3>Price Chart</h3>
+      </CardHeader>
+
+      <CardBody className={styles.cardBody}>
+        {isLoading && <Spinner />}
+
+        {!isLoading && (
+          <>
+            <h4 className={styles.priceTitle}>
+              GUIL/ETH {caret} {lastPrice}
+            </h4>
+
+            <div className={styles.priceChart}>
+              <Chart
+                options={chartOptions}
+                series={[{ data: serie }]}
+                type="candlestick"
+                height="100%"
+              />
+            </div>
+          </>
+        )}
+      </CardBody>
+    </Card>
+  );
+};
+
+export const PriceChart = observer(PriceChartComponent);
